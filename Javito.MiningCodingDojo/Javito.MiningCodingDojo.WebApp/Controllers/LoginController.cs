e9b3d5c31ec9a7b86f84fd1a0a9b1e0a02c5bac3
javitoHertfy <javierhertfelder@gmail.com>
2016-10-12 11:50:55 +0200

    Adding Applicaiton and Infrasctructure layer, we will save everything in memory

--- Javito.MiningCodingDojo/Javito.MiningCodingDojo.WebApp/Controllers/LoginController.cs
+++ Javito.MiningCodingDojo/Javito.MiningCodingDojo.WebApp/Controllers/LoginController.cs
@@ -4,11 +4,41 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Javito.MiningCodingDojo.ServiceLibrary;
+using Swashbuckle.Swagger.Annotations;
 
 namespace Javito.MiningCodingDojo.WebApp.Controllers
 {
     public class LoginController : ApiController
     {
+        private readonly MinerManagementAppService minerManagementAppService;
 
+        public LoginController()
+        {
+            this.minerManagementAppService = new MinerManagementAppService();
+        }
+
+        // POST api/values
+        [SwaggerOperation("CreateMiner")]
+        [SwaggerResponse(HttpStatusCode.Created)]
+        public void CreateMiner([FromBody]string name)
+        {
+
+        }
+
+        // POST api/values
+        [SwaggerOperation("LoginMine")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        public void LoginMine([FromBody]string name)
+        {
+
+        }
+
+        // GET api/values
+        [SwaggerOperation("GetAll")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
     }
 }
