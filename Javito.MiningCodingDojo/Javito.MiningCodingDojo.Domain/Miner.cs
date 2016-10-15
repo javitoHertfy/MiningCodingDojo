a0f782b6c8b4c71d6d8c6859704238f55d3aba3a
javitoHertfy <javierhertfelder@gmail.com>
2016-10-15 20:46:31 +0200

    Added Domain Project

---
+++ Javito.MiningCodingDojo/Javito.MiningCodingDojo.Domain/Miner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Javito.MiningCodingDojo.Domain
+{
+    public class Miner
+    {
+        public Miner(string name)
+        {
+            this.name = name;
+            this.id = Guid.NewGuid();
+            this.isLogged = false;
+            this.goldObtained = 0;
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+        private Guid id;
+        public Guid Id
+        {
+            get { return id; }
+            private set { id = value; }
+        }
+
+        private bool isLogged;
+        public bool IsLogged
+        {
+            get { return isLogged; }
+            private set { isLogged = value; }
+        }
+
+        private int goldObtained;
+        public int GoldObtained
+        {
+            get { return goldObtained; }
+            set { goldObtained = value; }
+        }
+    }
+}
